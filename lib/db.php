2061531069fd6132a06f0ab04eb71afd0a8e8677
ChristianeO <christiane.oubly@gmail.com>
2015-03-16 17:37:19 +0100

    chrichriii

--- lib/db.php
+++ lib/db.php
@@ -1,7 +1,7 @@
 <?php
 try{
-/*$db = new PDO('mysql:host=localhost;dbname=cinapps', 'root', '');*/
-$db = new PDO('mysql:host=sqletud.univ-mlv.fr;dbname=qgiraud_db', 'qgiraud', '140294');
+$db = new PDO('mysql:host=localhost;dbname=cinapps', 'root', '');
+/*$db = new PDO('mysql:host=sqletud.univ-mlv.fr;dbname=qgiraud_db', 'qgiraud', '140294');*/
 
 
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
