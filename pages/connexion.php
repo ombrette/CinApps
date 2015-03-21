c54846c943988da8a8656209d26cf0363f582588
seb <seb.lux17@gmail.com>
2015-03-21 11:53:00 +0100

    connexion inscription correction

--- pages/connexion.php
+++ pages/connexion.php
@@ -2,25 +2,27 @@
 $auth = 0;
 include 'lib/include.php';
 $title_page='Inscription';
-$adr='css/inscription.css';
+$adr='css/connexion.css';
 include 'partials/header.php'; ?>
 
 
     <!--INSCRIPTION-->
-<div class="container inscription">
+<div class="container connexion">
     <div class="row">
         <div class="col-lg-12 col-md-12 col-sm-12">
-            <h1>Créer un compte</h1>
-            <form id="inscription" method="post" action="#">
+            <h1>Se connecter</h1>
+            <form id="connexion" method="post" action="#">
                     <p><label for="pseudo">Entrez votre nom ou pseudo :</label><br><input type="text" id="pseudo" name="pseudo" placeholder="Mr Rabbit" /></p>             
-                    <p><label for="mail">Entrez votre adresse mail :</label><br><input type="email" id="mail" name="mail" placeholder="leroilapin@hotmail.fr" /></p>
                     <p><label for="password">Entrez votre mot de passe :</label><br><input type="password" id="password" name="password" placeholder="*****" /></p>
-                    <p><label for="password">Confirmez votre mot de passe :</label><br><input type="password" id="password" name="password" placeholder="*****" /></p>
                 
-                <input class="boutonfdj" type="submit" name="envoi" value="Confirmer" />
+                <input class="boutonfdj"t ype="submit" name="envoi" value="Confirmer" />
                 <a href="index.php"><p class="boutonfdj">Annuler</p></a><br><br>
-                
+
+                <a href="inscrption.php">S'inscrire</a>
                 <a href="index.php">Accéder sans se connecter</a>
+                <a href="#">Mot de passe oublié ?</a>
+
+
             </form>
         </div>
     </div>
