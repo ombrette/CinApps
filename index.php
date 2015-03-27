fa2968ea68deb6503ca9da2b2d65ee7c6e06d321
LAMBERT Héma <lamberthema@hotmail.fr>
2015-03-27 23:13:33 +0100

    index

--- index.php
+++ index.php
@@ -10,13 +10,10 @@
     <link href="css/style.css" rel="stylesheet">
 
     <link rel="stylesheet" type="text/css" href="css/slick.css"/>
-  <link rel="stylesheet" type="text/css" href="css/slick-theme.css"/>
-
-
+    <link rel="stylesheet" type="text/css" href="css/slick-theme.css"/>
 
     <!-- custom font -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
 
     <!--[if lt IE 9]>
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
@@ -24,57 +21,55 @@
 
     <link rel="icon" type="image/png" href="img/favicon.png" />
 
+    <link href="css/jquery.fs.shifter.css" rel="stylesheet" type="text/css" media="all">
 
- <script src='http://codepen.io/assets/libs/fullpage/jquery.js'></script>
-      
-    
-      <script src="js/main.js"></script>
-      <!-- http://codepen.io/anon/pen/gbWaZz -->
-    
-    
+    <!-- Add fancyBox -->
+    <link rel="stylesheet" href="css/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="js/bootstrap.min.js"></script>
-  
-        <link href="css/jquery.fs.shifter.css" rel="stylesheet" type="text/css" media="all">
-        <script src="js/jquery.fs.shifter.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
-<!-- Add fancyBox -->
-<link rel="stylesheet" href="css/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
-<script type="text/javascript" src="js/jquery.fancybox.pack.js?v=2.1.5"></script>
-<script type="text/javascript">
-    $(document).ready(function() {
+    <script type="text/javascript">
+        $(document).ready(function() {     
+            $("a.site").fancybox({          
+                'hideOnContentClick'        : true,
+                'padding'           : 0,
+                'overlayColor'          :'#D3D3D3',
+                'transitionIn'          :'elastic',
+                'transitionOut'         :'elastic',
+                'overlayOpacity'        : 0.7,
+                'zoomSpeedIn'           : 300,
+                'zoomSpeedOut'          : 300,
+                'width'             : 950,
+                'height'            : 400,
+                'type'              :'iframe'
+            });
+        });
+    </script>
         
 
-$("a.site").fancybox({          
-        'hideOnContentClick'        : true,
-        'padding'           : 0,
-        'overlayColor'          :'#D3D3D3',
-        'transitionIn'          :'elastic',
-        'transitionOut'         :'elastic',
-        'overlayOpacity'        : 0.7,
-        'zoomSpeedIn'           : 300,
-        'zoomSpeedOut'          : 300,
-        'width'             : 950,
-        'height'            : 400,
-        'type'              :'iframe'
-        });
-    });
-</script>
-    
-
-        <script>
-            $(document).ready(function() {
-                $.shifter({
-                    maxWidth: Infinity
-                });
+    <script>
+        $(document).ready(function() {
+            $.shifter({
+                maxWidth: Infinity
             });
-        </script>
+        });
+    </script>
 
 </head>
 
 <body class="shifter">
+
+<?php 
+//Connexion à la base de données 
+include 'lib/db.php';
+
+$requete = $db -> query("SELECT film.id, film.titre, film.affiche, film.note, film.synopsisCourt, film.trailer, genre.nom, YEAR(date) FROM film, film_genre, genre WHERE film.titre='Shutter Island' AND film.id=film_genre.id_film AND film_genre.id_genre=genre.id LIMIT 1");
+$result=$requete->fetchAll();
+
+$req_recomm=$db->query("SELECT film.id, film.titre, film.affiche FROM film, recommandation WHERE film.id=recommandation.film_id");
+$recommandations=$req_recomm->fetchAll();
+?>
 
     <div class="shifter-page">
         <header class="visible-xs menu-mobile">
@@ -87,134 +82,91 @@ $("a.site").fancybox({
             </div>
         </header>
 
-    <?php
-
-    include 'lib/db.php';
-
-    ?>
-
-<?php
-//Connexion à la base de données 
-
-$requete = $db -> query("SELECT * FROM film, film_genre, genre WHERE film.titre='Shutter Island' AND film.id=film_genre.id_film AND film_genre.id_genre=genre.id LIMIT 1");
-$result=$requete->fetchAll();
-
-$req_recomm=$db->query("SELECT film.id, film.titre, film.affiche FROM film, recommandation WHERE film.id=recommandation.film_id");
-$recommandations=$req_recomm->fetchAll();
-?>
 
     <!--MENU-->
     
     <nav class="navbar navbar-default hidden-xs" role="navigation">
     
-            <div class="container">
-                
-                        
-                <div class="navbar-header">
-
-                    <h4 class="visible-xs">Cinéos</h4>
+        <div class="container">
+            
                     
-                </div>
-    
+            <div class="navbar-header">
+
+                <h4 class="visible-xs">Cinéos</h4>
+                
+            </div>
+
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <a class="navbar-brand hidden-xs" href="">
-                        <img src="img/logo2.png" width="38" height="50" alt="logo">Cinéos</a>
+                    <img src="img/logo2.png" width="38" height="50" alt="logo">
+                    <p class="nom_logo">Cinéos</p>
+                </a>
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="index.php">ACCUEIL</a></li>
                     <li><a href="pages/questionnaire.php">QUESTIONNAIRE</a></li>
                     <li><a href="pages/liste_films.php">LISTE DE FILMS</a></li>
                     <li><a href="pages/profil.php">MON COMPTE</a></li>
-                                   
 
                     <li class="recherche"><a href="#"><img class="logorecherche" src="img/search.png" width="47" height="60" alt="Logo Recherche"></a></li>
                 </ul>
             </div>
-    
-        </nav>
+        </div>
+
+    </nav>
     
     <!--FIN MENU-->
 
 
 
     <!--FILM DU JOUR-->
-<div class="container fdj marg-top">
+    <div class="container fdj marg-top">
+        <?php foreach($result as $res): ?>
+        <div id="general" class="row">
+            <div class="col-lg-4 col-md-4 col-sm-4">
+                <h1 class="titre-section text-uppercase">Film du Jour</h1>
+                <img src="<?= $res['affiche'] ?>" class="img-responsive center-block" alt="">
+            </div>
 
-    <div id="general" class="row">
-        <div class="col-lg-4 col-md-4 col-sm-4">
-            <?php foreach($result as $res): ?>
-            <h1 class="titre-section text-uppercase">Film du Jour</h1>
-            <img src="<?= $res['affiche'] ?>" class="img-responsive center-block" alt="">
-            <?php endforeach ?>
-        </div>
+            <div class="col-lg-7 col-md-7 col-sm-7 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 contenu">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <h2 class="titre-film"><?= $res['titre'] ?></h2>
+                    </div>
 
-        <div class="col-lg-7 col-md-7 col-sm-7 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 contenu">
-            
-            <div class="row">
-
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-
-                <?php foreach($result as $res): ?>
-                <h2 class="titre-film"><?= $res['titre'] ?></h2></div>
-
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                <p class="note" title="<?= $res['note'] ?> / 5" alt="<?= $res['note'] ?> / 5">
-                    <?php include 'lib/note.php'; ?>
-                </p>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <p class="note" title="<?= $res['note'] ?> / 5" alt="<?= $res['note'] ?> / 5">
+                            <?php include 'lib/note.php'; ?>
+                        </p>
+                    </div>
                 </div>
 
-           </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <p class="infos gris">Année : <?= $res['YEAR(date)']; ?></p>
 
-            <div class="row">
-
-                <div class="col-lg-12">
-                <p class="infos gris">Année : </p>
-                
-
-                
-                <p class="infos gris">Genre : <?= $res['nom']; ?></p>
-                
-                <p class="text-justify description hidden-xs"><?= $res['synopsis']; ?></p>
-                </div>
-                
-
-                
-                <div class="col-lg-6 col-md-6 col-sm-6">
+                        <p class="infos gris">Genre : <?= $res['nom']; ?></p>
                     
-                <a href="<?= $res['trailer'] ?>" class="site video"><p class="boutonfdj"><i class="fa fa-play-circle-o"></i>Bande annonce</p></a>
-                    <!--<div style="display:none">
-                        <div id="video">
-                        
-<?= $res['trailer'] ?>
-                        
-                        </div>
-                    </div>-->
+                        <p class="text-justify description hidden-xs"><?= $res['synopsisCourt']; ?></p>
+                    </div>
+                     
+                    <div class="col-lg-6 col-md-6 col-sm-6">
+                        <a href="<?= $res['trailer'] ?>" class="site video"><p class="boutonfdj"><i class="fa fa-play-circle-o"></i>Bande annonce</p></a>
+                        <!--<div style="display:none">
+                            <div id="video">
+                                <?= $res['trailer'] ?>
+                            
+                            </div>
+                        </div>-->
+                    </div> 
 
-
-
-                </div> 
-
-
-                <div class="col-lg-6 col-md-6 col-sm-6">
-                    <a href="#"><p class="boutonfdj"><i class="fa fa-file-text-o"></i>A regarder plus tard</p></a>
-                </div>
-
-
-             </div>
-
-
-<?php endforeach ?>
-
-            
-
-
+                    <div class="col-lg-6 col-md-6 col-sm-6">
+                        <a href="#"><p class="boutonfdj"><i class="fa fa-file-text-o"></i>A regarder plus tard</p></a>
+                    </div>
+                 </div>
+            </div>
         </div>
-
-
+        <?php endforeach ?>
     </div>
-
-
-
-</div>
     
     <!--FIN FILM DU JOUR-->
     
@@ -229,27 +181,20 @@ $recommandations=$req_recomm->fetchAll();
     
     
         <div class="center">
-          <!--<div><a href="pages/fiche_film.php"><img src="img/malefique2.jpg"><h3 class="text-center">Maléfique</h3></a><p class="text-center">Le Lornonyme assembla</p></div>
-           <div><a href="pages/fiche_film.php"><img src="img/malefique2.jpg"><h3 class="text-center">Maléfique</h3></a><p class="text-center">Le Lornonyme assembla</p></div>
-            <div><a href="pages/fiche_film.php"><img src="img/malefique2.jpg"><h3 class="text-center">Maléfique</h3></a><p class="text-center">Le Lornonyme assembla</p></div>
-             <div><a href="pages/fiche_film.php"><img src="img/malefique2.jpg"><h3 class="text-center">Maléfique</h3></a><p class="text-center">Le Lornonyme assembla</p></div>
-              <div><a href="pages/fiche_film.php"><img src="img/malefique2.jpg"><h3 class="text-center">Maléfique</h3></a><p class="text-center">Le Lornonyme assembla</p></div>
-               <div><a href="pages/fiche_film.php"><img src="img/malefique2.jpg"><h3 class="text-center">Maléfique</h3></a><p class="text-center">Le Lornonyme assembla</p></div>
-                <div><a href="pages/fiche_film.php"><img src="img/malefique2.jpg"><h3 class="text-center">Maléfique</h3></a><p class="text-center">Le Lornonyme assembla</p></div>
-                 <div><a href="pages/fiche_film.php"><img src="img/malefique2.jpg"><h3 class="text-center">Maléfique</h3></a><p class="text-center">Le Lornonyme assembla</p></div>-->
         <?php foreach ($recommandations as $recommandation): ?>
-          <div><a href="pages/fiche_film.php?id=<?= $recommandation['id'] ?>"><img src="<?= $recommandation['affiche'] ?>" class="img-responsive"><h3 class="text-center"><?= $recommandation['titre'] ?></h3></a><p class="text-center">Le Lornonyme assembla</p></div>
-            <?php endforeach ?>
-
-
+            <div>
+                <a href="pages/fiche_film.php?id=<?= $recommandation['id'] ?>"><img src="<?= $recommandation['affiche'] ?>" class="img-responsive">
+                    <h3 class="text-center"><?= $recommandation['titre'] ?></h3>
+                </a>
+                <p class="text-center">Le Lornonyme assembla</p>
+            </div>
+        <?php endforeach ?>
         </div>
 
     </div>
 
 
 
-
-   <script type="text/javascript" src="js/slick.min.js"></script>
 
   <script type="text/javascript">
     $(document).ready(function(){
@@ -297,6 +242,19 @@ $recommandations=$req_recomm->fetchAll();
             <a href="pages/profil.php"><img src="img/menu_profil.png" width="24" height="24" alt="logo" class="pull-left icon-menu-xs">Mon compte</a>
             <a href="pages/profil.php"><img src="img/menu_deconnexion.png" width="24" height="24" alt="logo" class="pull-left icon-menu-xs">Déconnexion</a>
         </nav>
+    <script src='http://codepen.io/assets/libs/fullpage/jquery.js'></script>  
+    
+    <script src="js/main.js"></script>
+    <!-- http://codepen.io/anon/pen/gbWaZz -->
+    
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.fs.shifter.js"></script>
+    
+    <script type="text/javascript" src="js/jquery.fancybox.pack.js?v=2.1.5"></script>
+    
+    <script type="text/javascript" src="js/slick.min.js"></script>
+
 </body>
 
 
